b743dfcfab12e6f07e5c2d6840bc713604e467eb
elise <hei@elisejakob.no>
2024-10-15 12:06:50 +0200

    update bios

--- web/src/components/PersonList.tsx
+++ web/src/components/PersonList.tsx
@@ -12,14 +12,14 @@ export default function PersonList() {
           email="jonas@datadyr.no"
           avatarUrl="/avatars/jonas.png"
           avatarAlt="Pikselavatar av Jonas"
-          description="Liker datamaskiner. Både frontend og backend, integrasjoner og migrasjoner. Blir glad for nye tekniske utfordringer."
+          description="Jonas tar seg av både frontend og backend, integrasjoner og migrasjoner. Han blir alltid glad for nye tekniske utfordringer, og bidrar med lure tanker og gode råd."
         />
         <Person
           name="Elise Jakobsen"
           email="elise@datadyr.no"
           avatarUrl="/avatars/elise.png"
           avatarAlt="Pikselavatar av Elise"
-          description="Liker datamaskiner og design og gjerne kombo. Snakker både utviklerne og designernes språk og har øye for designdetaljene."
+          description="Elise tar seg av frontend med et ekstra godt øye for designdetaljene, med bakgrunn som designer fra KHiO. Hun er ekstra glad i å få leke med gridsystemer og mikroanimasjoner."
         />
       </div>
     </section>
